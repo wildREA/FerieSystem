148e23e97c42cd1ede7ce529dbefef9ebaf98a4c
wildREA <wildrealinit@gmail.com>
2025-06-26 12:15:41 +0200

    Change container design

--- app/Views/components/key_container.php
+++ app/Views/components/key_container.php
@@ -4,17 +4,19 @@
 
 <div class="key-container">
     <div class="key-display">
-        <div class="key-label">API Key</div>
-        <div class="key-value-wrapper">
-            <input type="text" id="key_value" class="key-input" value="" readonly placeholder="Click generate to create key">
-            <div class="key-controls">
-                <button id="visibility" class="btn-key-control" title="Show/Hide Key">
-                    <i class="fas fa-eye"></i>
-                </button>
-                <button id="generate_key" class="btn-key-control btn-generate" title="Generate New Key">
-                    <i class="fas fa-key"></i>
-                </button>
+        <div class="key-section">
+            <div class="key-info">
+                <div class="key-label">API Key</div>
+                <div class="key-status" id="key_status">No key generated</div>
             </div>
+        </div>
+        <div class="key-controls">
+            <button id="visibility" class="btn-key-control" title="Show/Hide Key" style="display: none;">
+                <i class="fas fa-eye"></i>
+            </button>
+            <button id="generate_key" class="btn-key-control btn-generate" title="Generate New Key">
+                <i class="fas fa-plus"></i>
+            </button>
         </div>
     </div>
 </div>
